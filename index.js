b773cfa94a38bf5dc76b0f2797752d6a55f87473
mrana007 <rana.es.bc@gmail.com>
2023-10-18 06:59:13 +0600

    Implement app.get to fetch data from the MongoDB database

--- index.js
+++ index.js
@@ -28,7 +28,11 @@ async function run() {
 
     const productCollection = client.db("productDB").collection('products');
 
-    
+    app.get("/products", async(req, res) =>{
+        const cursor = productCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    });
 
     app.post("/products", async (req, res) => {
       const newProduct = req.body;
