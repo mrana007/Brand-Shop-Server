97ccff73bcf23b5cf8ea009dbc1029f32be318f9
mrana007 <rana.es.bc@gmail.com>
2023-10-19 02:08:24 +0600

    input products data in server and stored to MongoDB

--- index.js
+++ index.js
@@ -26,14 +26,36 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const brandCollection = client.db("productDB").collection('brands');
     const productCollection = client.db("productDB").collection('products');
 
+    app.get("/brands", async(req, res) =>{
+        const cursor = brandCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    });
+    // product add
     app.get("/products", async(req, res) =>{
         const cursor = productCollection.find();
         const result = await cursor.toArray();
         res.send(result);
     });
 
+    // show product brands base
+    app.get('/products/:brandName', async (req, res) => {
+        const brandName = req.params.brandName;
+        const query = { brand: brandName }
+        const result = await productCollection.find(query).toArray();
+        res.send(result);
+    })
+
+    app.post("/brands", async (req, res) => {
+      const newProduct = req.body;
+      console.log(newProduct);
+      const result = await brandCollection.insertOne(newProduct);
+      res.send(result);
+    });
+    // product post
     app.post("/products", async (req, res) => {
       const newProduct = req.body;
       console.log(newProduct);
